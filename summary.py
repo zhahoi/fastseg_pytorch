4c5678012b4c71abd87d432d9985cda122baa8fc
HongyingZhang <104057195+zhahoi@users.noreply.github.com>
2024-07-15 10:58:23 +0800

    Add files via upload

---
+++ summary.py
@@ -0,0 +1,34 @@
+#--------------------------------------------#
+#   该部分代码用于看网络结构
+#--------------------------------------------#
+import torch
+from thop import clever_format, profile
+from torchsummary import summary
+
+from nets.fastseg import MobileV3Large, MobileV3Small
+
+if __name__ == "__main__":
+    input_shape     = [416, 416]
+    num_classes     = 3
+    num_filters     = 128
+    large_backbone  = False
+    
+    device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if large_backbone:
+        model   = MobileV3Large(num_classes=num_classes, num_filters=num_filters).to(device)
+    else:
+        model   = MobileV3Small(num_classes=num_classes, num_filters=num_filters).to(device)
+    summary(model, (3, input_shape[0], input_shape[1]))
+    
+    dummy_input     = torch.randn(1, 3, input_shape[0], input_shape[1]).to(device)
+    flops, params   = profile(model.to(device), (dummy_input, ), verbose=False)
+    #--------------------------------------------------------#
+    #   flops * 2是因为profile没有将卷积作为两个operations
+    #   有些论文将卷积算乘法、加法两个operations。此时乘2
+    #   有些论文只考虑乘法的运算次数，忽略加法。此时不乘2
+    #   本代码选择乘2，参考YOLOX。
+    #--------------------------------------------------------#
+    flops           = flops * 2
+    flops, params   = clever_format([flops, params], "%.3f")
+    print('Total GFLOPS: %s' % (flops))
+    print('Total params: %s' % (params))
